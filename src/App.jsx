c9064fa544d7c7fce1c8d0a042f2f9a3e9f3ae24
Pratik1618 <kadampratik18@gmail.com>
2025-03-29 17:31:43 +0530

    eod

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { Route,BrowserRouter as  Router, Routes, useNavigate } from "react-router-dom";
+import Schedule from "./pages/schedule";
 const App = () => {
   const [open, setOpen] = useState(true);
   const [selectedTile, setSelectedTile] = useState("Home Page");
+  const navigate = useNavigate();
   const Menus = [
     { title: "Dashboard", src: "Chart_fill",path:'' },
     { title: "Inbox", src: "Chat" },
     { title: "Accounts", src: "User", gap: true },
-    { title: "Schedule ", src: "Calendar" },
+    { title: "Schedule ", src: "Calendar" ,path:"/schedule"},
     { title: "Search", src: "Search" },
     { title: "Analytics", src: "Chart" },
     { title: "Files ", src: "Folder", gap: true },
@@ -14,6 +17,9 @@ const App = () => {
   ];
 
   return (
+
+
+  
     <div className="flex">
       <div
         className={` ${
@@ -22,7 +28,7 @@ const App = () => {
       >
         <img
           src="./src/assets/control.png"
-          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+          className={`absolute cursor-pointer -right-3 top-9 w-7 border-purple-700
           border-2 rounded-full  ${!open && "rotate-180"}`}
           onClick={() => setOpen(!open)}
         />
@@ -45,11 +51,13 @@ const App = () => {
           {Menus.map((Menu, index) => (
             <li
               key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+              className={`flex  rounded-md p-2 cursor-pointer hover:bg-stone-200/50 text-gray-300 text-sm items-center gap-x-4 
               ${Menu.gap ? "mt-9" : "mt-2"} ${
-                index === 0 && "bg-light-white"
+                selectedTile === Menu.title ? "bg-stone-200/50":""
               } `}
-              onClick={() => setSelectedTile(Menu.title)}
+              onClick={() =>{ setSelectedTile(Menu.title); 
+                navigate(Menu.path)}}
+              
             >
               <img src={`./src/assets/${Menu.src}.png`} />
               <span className={`${!open && "hidden"} origin-left duration-200`}>
@@ -60,10 +68,18 @@ const App = () => {
         </ul>
       </div>
       <div className="h-screen flex-1 p-7">
-        <h1 className="text-2xl font-semibold ">{selectedTile}</h1>
+
+     
+        <Routes>
+          <Route path="/schedule" element={<Schedule/>}/>
+          <Route path="/" element={<h1>Welcome to Dashboard</h1>} />
+        </Routes>
         
       </div>
     </div>
+
+    
+
   );
 };
 export default App;
---
+++ src/pages/Schedule.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Schedule = () => {
+  return (
+    <div>
+         <h1 className="text-2xl font-semibold ">Schedule</h1>
+    </div>
+  )
+}
+
+export default Schedule
